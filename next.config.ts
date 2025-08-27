240d7df11d0662024bf54728552dbf6e256f25a3
Jovanni Jungco <jovannijungco27@gmail.com>
2025-08-27 22:28:55 +0800

    fix cors issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   crossOrigin: "anonymous",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "http://aecec2929917a4a1792f13b25766bfa6-1524793708.ap-southeast-1.elb.amazonaws.com/:path*",
+      },
+    ];
+  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
