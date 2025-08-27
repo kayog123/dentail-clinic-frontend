c9fa70c28d84b2604531da9ccd99172c18a0ce1c
Jovanni Jungco <jovannijungco27@gmail.com>
2025-08-27 22:12:51 +0800

    fix cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  crossOrigin: "anonymous",
   reactStrictMode: false,
   images: {
     remotePatterns: [
--- src/app/components/query-provider.tsx
+++ src/app/components/query-provider.tsx
@@ -7,37 +7,37 @@ import { useState } from "react";
 interface QueryProviderProps {
   children: React.ReactNode;
 }
-
+const queryClient = new QueryClient();
 export default function QueryProvider({ children }: QueryProviderProps) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000, // 1 minute
-            gcTime: 10 * 60 * 1000, // 10 minutes
-            retry: (failureCount, error: unknown) => {
-              // Don't retry on 4xx errors
-              if (
-                error &&
-                typeof error === "object" &&
-                "status" in error &&
-                typeof error.status === "number"
-              ) {
-                if (error.status >= 400 && error.status < 500) {
-                  return false;
-                }
-              }
-              return failureCount < 3;
-            },
-            refetchOnWindowFocus: false,
-          },
-          mutations: {
-            retry: 1,
-          },
-        },
-      })
-  );
+  // const [queryClient] = useState(
+  //   () =>
+  //     new QueryClient({
+  //       defaultOptions: {
+  //         queries: {
+  //           staleTime: 60 * 1000, // 1 minute
+  //           gcTime: 10 * 60 * 1000, // 10 minutes
+  //           retry: (failureCount, error: unknown) => {
+  //             // Don't retry on 4xx errors
+  //             if (
+  //               error &&
+  //               typeof error === "object" &&
+  //               "status" in error &&
+  //               typeof error.status === "number"
+  //             ) {
+  //               if (error.status >= 400 && error.status < 500) {
+  //                 return false;
+  //               }
+  //             }
+  //             return failureCount < 3;
+  //           },
+  //           refetchOnWindowFocus: false,
+  //         },
+  //         mutations: {
+  //           retry: 1,
+  //         },
+  //       },
+  //     })
+  // );
 
   return (
     <QueryClientProvider client={queryClient}>
